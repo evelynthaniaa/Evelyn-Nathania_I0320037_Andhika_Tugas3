8a3964439a2b668db1af2e21316e24c3ab291c23
evelynthaniaa <80144798+evelynthaniaa@users.noreply.github.com>
2021-03-18 17:48:55 +0700

    Add files via upload

---
+++ I0320037_Soal1_Tugas3.py
@@ -0,0 +1,20 @@
+# List 10 teman
+list = ['Hasan','Audrey','Litha','Angela','Krisna','Adrian','Alya','Rifa','Hera','Zea']
+
+# Menampilkan list indeks nomor 4,6, dan 7
+print("List indeks nomor 4,6, dan 7 adalah",list[4],",",list[6],", dan",list[7])
+
+# Mengganti nama teman di list 3,5, dan 9
+list[3] = 'Fadhila'
+list[5] = 'Fika'
+list[9] = 'Azizah'
+
+# Menambahkan 2 nama teman
+list.append('Vita')
+list.append('Asha')
+
+# Menampilkan semua teman dengan pengulangan
+print(list*2)
+
+# Menampilkan panjang list
+print(len(list))
---
+++ I0320037_Soal2_Tugas3.py
@@ -0,0 +1,9 @@
+# Membuat dictionary
+dict = {'Nama':'Evelyn','Hobi1':'Bernyanyi','Hobi2':'Membaca buku','Hobi3':'Memasak','Sosmed1':'ig:@evelynthaniaa','Sosmed2':'line:evelynnathaniaa','Sosmed3':'twitter:@evelynthaniaa','Lagu1':'Little Things','Lagu2':'Somewhere only we know','Lagu3':'Jikalau','Makanan1':'Pempek','Makanan2':'Nasi goreng','Makanan3':'Coklat'}
+
+# Mengubah salah satu hobi dan sosmed, menghapus 2 makanan, dan menambah 1 hobi
+dict['Hobi2'] = 'Menjahit'
+dict['Sosmed1'] = 'ig:@epe_linn'
+del dict['Makanan1']
+del dict['Makanan2']
+dict['Hobi4'] = 'Menggambar'
